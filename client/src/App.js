6b78b89a5f2ad9e173bbf8237a67c91d51f2df90
devCoderian <devcoderian@gmail.com>
2021-09-09 21:36:25 +0900

    edit restApi & proxy

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Customer from './components/Customer';
 import { Table, TableHead, TableRow, TableCell, TableBody} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { Paper } from '@material-ui/core';
+import { useEffect, useLayoutEffect, useState } from 'react';
 const styles = theme => ({
   root:{
     width:'100%',
@@ -15,33 +16,51 @@ const styles = theme => ({
   }
 })
 
-const customers =[{
-  'id': 1,
-  'image': 'http://placeimg.com/64/64/any',
-  'name': '유저1',
-  'birth':'960719',
-  'gender':'여',
-  'job':'무직'
-},
-{
-  'id': 2,
-  'image': 'http://placeimg.com/64/64/any',
-  'name': '유저2',
-  'birth':'960719',
-  'gender':'여',
-  'job':'무직'
-},
-{
-  'id': 3,
-  'image': 'http://placeimg.com/64/64/any',
-  'name': '유저3',
-  'birth':'960719',
-  'gender':'여',
-  'job':'무직'
-}];
+// const customers =[{
+//   'id': 1,
+//   'image': 'http://placeimg.com/64/64/any',
+//   'name': '유저1',
+//   'birth':'960719',
+//   'gender':'여',
+//   'job':'무직'
+// },
+// {
+//   'id': 2,
+//   'image': 'http://placeimg.com/64/64/any',
+//   'name': '유저2',
+//   'birth':'960719',
+//   'gender':'여',
+//   'job':'무직'
+// },
+// {
+//   'id': 3,
+//   'image': 'http://placeimg.com/64/64/any',
+//   'name': '유저3',
+//   'birth':'960719',
+//   'gender':'여',
+//   'job':'무직'
+// }];
+//-> server.js 로 이동 
 
 function App(props) {
   const {classes} = props; 
+
+  const [customers, setCustomers] = useState([]);
+
+  const callApi = async () =>{
+    const res = await fetch('/api/customers',{
+      headers:{
+        'Accept':'application /json'
+      }
+    });
+    const body = await res.json();
+    return body;
+  }
+  useEffect(()=>{
+    console.log('실행');
+    callApi().then(res => setCustomers(res));
+  },[]);
+
   return(
   // 메인 자바스크립트 관리
     <Paper className={classes.root}>
@@ -57,19 +76,20 @@ function App(props) {
          </TableRow>
        </TableHead>
         <TableBody>
-        {customers.map((customer)=>{
-          return(
-            <Customer
-                key ={customer.id}
-                id = {customer.id}
-                image = {customer.image}
-                name = {customer.name}
-                birth = {customer.birth}
-                gender = {customer.gender}
-                job= {customer.job}
-          />
-          )
-        })
+        
+        {customers?customers.map((customer)=>{
+            return(
+              <Customer
+                  key ={customer.id}
+                  id = {customer.id}
+                  image = {customer.image}
+                  name = {customer.name}
+                  birth = {customer.birth}
+                  gender = {customer.gender}
+                  job= {customer.job}
+              />
+            )
+        }):""
       }
       </TableBody>
       </Table>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.get('/api/hello', (req, res)=>{
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res)=>{
+    res.send([
+        {
+            'id': 1,
+            'image': 'http://placeimg.com/64/64/any',
+            'name': '유저1',
+            'birth':'960719',
+            'gender':'여',
+            'job':'무직'
+          },
+          {
+            'id': 2,
+            'image': 'http://placeimg.com/64/64/any',
+            'name': '유저2',
+            'birth':'960719',
+            'gender':'여',
+            'job':'무직'
+          },
+          {
+            'id': 3,
+            'image': 'http://placeimg.com/64/64/any',
+            'name': '유저3',
+            'birth':'960719',
+            'gender':'여',
+            'job':'무직'
+          }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening server ${port}`));
